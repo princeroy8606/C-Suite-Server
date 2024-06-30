ba334d071954267f5afb81adac6e321acc9163d8
PRINCE ROY <83387075+princeroy8606@users.noreply.github.com>
2024-06-30 22:46:15 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,15 +3,18 @@ const User = require("../models/user");
 exports.registerUser = async (req, res) => {
   const { name, authId, email } = req.body;
   try {
-    const user = new User({
-      name,
-      authId,
-      email,
-    });
-    await user.save();
-    res.status(200).json(user);
+    const existingUser = await User.findOne(email);
+    if (!existingUser) {
+      const user = new User({
+        name,
+        authId,
+        email,
+      });
+      await user.save();
+      res.status(200).json(user);
+    }
+    res.status(200).json({ ok: true });
   } catch (error) {
     res.status(400).json(error);
   }
 };
-
