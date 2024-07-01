f483a6be88c1fc06839b0fd8c4aa548c51495a5b
PRINCE ROY <83387075+princeroy8606@users.noreply.github.com>
2024-07-01 18:06:07 +0530

    add -> linkedin auth

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,20 +1,68 @@
+const { default: axios } = require("axios");
 const User = require("../models/user");
 
+const clientId = "86aue2n06v0823";
+const clientSecret = "L7YBEDGd4b08vFEe";
+const redirectUri = "http://localhost:3000/auth-linkedin-bridge";
+
 exports.registerUser = async (req, res) => {
-  const { name, authId, email } = req.body;
+  let { name, authId, email, methord, Code } = req.body;
+  console.log(Code);
+  if (methord === "linkedin") {
+    console.log("execution-start");
+    try {
+      const { data } = await axios.post(
+        "https://www.linkedin.com/oauth/v2/accessToken",
+        null,
+        {
+          params: {
+            grant_type: "authorization_code",
+            code: Code,
+            redirect_uri: redirectUri,
+            client_id: clientId,
+            client_secret: clientSecret,
+          },
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      console.log("data-token", data);
+      if (data) {
+        const response = await axios.get(
+          "https://api.linkedin.com/v2/userinfo",
+          {
+            headers: { Authorization: `Bearer ${data.access_token}` },
+          }
+        );
+        if (response?.data) {
+          (name = response?.data?.name),
+            (email = response?.data?.email),
+            (authId = response?.data?.sub);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(error);
+    }
+  }
   try {
-    const existingUser = await User.findOne(email);
+    console.log("execute");
+    const existingUser = await User.findOne({ email: email });
     if (!existingUser) {
       const user = new User({
         name,
         authId,
         email,
       });
+      console.log(user);
       await user.save();
       res.status(200).json(user);
+    } else {
+      res.status(200).json({ ok: true });
     }
-    res.status(200).json({ ok: true });
   } catch (error) {
+    console.log(error);
     res.status(400).json(error);
   }
 };
